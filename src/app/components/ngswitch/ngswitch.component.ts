3d84429d0dc0378a4b9a6e45831ae1dcf858f09a
MAGM\Mario <mariogtzmdza@gmail.com>
2018-04-01 20:53:46 -0500

    first commit

---
+++ src/app/components/ngswitch/ngswitch.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngswitch',
+  templateUrl: './ngswitch.component.html',
+  styles: []
+})
+export class NgswitchComponent implements OnInit {
+
+  estado:string = "success";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
